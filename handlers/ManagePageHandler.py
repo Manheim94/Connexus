421d013070bb48cab082e4ec625eaf5c2a644d5b
victor-zhang-000 <31371987+victor-zhang-000@users.noreply.github.com>
2017-10-01 17:48:46 -0500

    test url

--- handlers/ManagePageHandler.py
+++ handlers/ManagePageHandler.py
@@ -4,6 +4,10 @@ from config import utils
 from services.TestServiceHandler import TestServiceHandler
 import json
 
+from google.appengine.api import urlfetch
+from google.appengine.api import app_identity
+import urllib
+
 class ManagePageHandler(webapp2.RequestHandler):
     def get(self):
         if not users.get_current_user():
@@ -12,13 +16,31 @@ class ManagePageHandler(webapp2.RequestHandler):
             self.render()
 
     def render(self):
+
+        appName = app_identity.get_application_id()
+        current_user = users.get_current_user().email()
+        logout_url = users.create_logout_url(utils.raw_logout_url)
+
+        try:
+            rpc = urlfetch.create_rpc()
+            request = {}
+            request['user_id'] = current_user
+            request['stream_id'] = 'stream1'
+
+            url = 'https://services.' + appName + '.appspot.com?' + urllib.urlencode(request)
+            urlfetch.make_fetch_call(rpc, url)
+            response = rpc.get_result()
+
+        except Exception:
+            self.response.write("General error!<br>")
+            self.response.write(Exception)
+        '''
         instance= TestServiceHandler()
         data= json.loads(instance.getJson())
         owned_stream_list= data['owned_stream_list']
-
-        logout_url = users.create_logout_url(utils.raw_logout_url)
-
-        current_user = users.get_current_user().email()
+        '''
+        data= json.loads(response)
+        owned_stream_list=data['owned_stream_list']
 
         template_values = {
             'logout_url': logout_url,
--- handlers/ViewSingleStreamHandler.py
+++ handlers/ViewSingleStreamHandler.py
@@ -2,7 +2,12 @@
 
 import webapp2
 from google.appengine.api import users
+from google.appengine.api import urlfetch
+from google.appengine.api import app_identity
+
 from config import utils
+import urllib
+
 
 class ViewSingleStreamHandler(webapp2.RequestHandler):
     def get(self):
@@ -12,6 +17,24 @@ class ViewSingleStreamHandler(webapp2.RequestHandler):
             self.render()
 
     def render(self):
+        appName = app_identity.get_application_id()
+
+        try:
+            rpc = urlfetch.create_rpc()
+            request={}
+            request['user_id']=users.get_current_user().email()
+            request['stream_id'] = 'stream1'
+
+            url = 'https://services.' + appName + '.appspot.com?' + urllib.urlencode(request)
+            urlfetch.make_fetch_call(rpc, url)
+            response = rpc.get_result()
+
+        except Exception:
+            self.response.write("General error!<br>")
+            self.response.write(Exception)
+
+
+
         logout_url = users.create_logout_url(utils.raw_logout_url)
         template_values = {
             'logout_url': logout_url,
