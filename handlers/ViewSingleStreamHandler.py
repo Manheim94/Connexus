efc1024b2ed4bf5f69030552910b00f906c67c4e
victor-zhang-000 <31371987+victor-zhang-000@users.noreply.github.com>
2017-10-02 19:55:11 -0500

    create test good

--- handlers/ViewSingleStreamHandler.py
+++ handlers/ViewSingleStreamHandler.py
@@ -7,38 +7,47 @@ from google.appengine.api import app_identity
 
 from config import utils
 import urllib
+import json
 
 
 class ViewSingleStreamHandler(webapp2.RequestHandler):
+    stream_id=""
     def get(self):
         if not users.get_current_user():
             self.redirect(users.create_login_url(self.request.uri))
         else:
+            stream_id = self.request.get('stream_id')
             self.render()
 
     def render(self):
-        appName = app_identity.get_application_id()
+        #appName = app_identity.get_application_id()
+        current_user = users.get_current_user().email()
+        logout_url = users.create_logout_url(utils.raw_logout_url)
+        data = {}
 
         try:
             rpc = urlfetch.create_rpc()
-            request={}
-            request['user_id']=users.get_current_user().email()
-            request['stream_id'] = 'stream1'
+            request = {}
+            request['user_id'] = current_user
+            request['stream_id'] = ViewSingleStreamHandler.stream_id
+            url= 'https://services-dot-hallowed-forge-181415.appspot.com/service-viewsinglestream?' + urllib.urlencode(request)
 
-            url = 'https://services.' + appName + '.appspot.com?' + urllib.urlencode(request)
             urlfetch.make_fetch_call(rpc, url)
             response = rpc.get_result()
+            data = json.loads(response.content)
 
         except Exception:
-            self.response.write("General error!<br>")
+            self.response.write("Error!<br>")
             self.response.write(Exception)
 
-
-
+        pict_list=data['pict_list']
+        page_range=data['page_range']
         logout_url = users.create_logout_url(utils.raw_logout_url)
         template_values = {
             'logout_url': logout_url,
-            'current_user': 'haha'
+            'current_user': current_user,
+            'pict_list':pict_list,
+            'page_range':page_range
         }
         template = utils.JINJA_ENVIRONMENT.get_template('fresh_manage.html')
         self.response.write(template.render(template_values))
