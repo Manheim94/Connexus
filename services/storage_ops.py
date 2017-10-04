0b9b1b42f134791377453164e4fcc12769b71c7f
yaohua <628zhao@gmail.com>
2017-10-04 14:50:59 -0500

    modify ops, change storage-ops, fix bugs in serviceHandlers

--- services/storage_ops.py
+++ services/storage_ops.py
@@ -7,19 +7,8 @@ import lib.cloudstorage as gcs
 from google.appengine.api import images
 
 
-def create_stream_in_storage(self, stream_name):
-    bucket_name = os.environ.get('BUCKET_NAME', app_identity.get_default_gcs_bucket_name())
-    self.response.headers['Content-Type'] = 'text/plain'
-    self.response.write('Demo GCS Application running from Version: '
-                        + os.environ['CURRENT_VERSION_ID'] + '\n')
-    self.response.write('Using bucket name: ' + bucket_name + '\n\n')
-    bucket = '/' + bucket_name
-    filename = bucket + '/' + str(stream_name) + "/"
-
-    self.response.write('Creating file %s\n' % filename)
-    write_retry_params = gcs.RetryParams(backoff_factor=1.1)
-    gcs_file = gcs.open(filename, 'w',
-                        options={'x-goog-meta-foo': 'foo', 'x-goog-meta-bar': 'bar'},
-                        retry_params=write_retry_params)
+def create_stream_in_storage(stream_name):
+    bucket = "/hallowed-forge-181415.appspot.com/"
+    filename = bucket + str(stream_name) + "/"
+    gcs_file = gcs.open(filename, 'w')
     gcs_file.close()
-
